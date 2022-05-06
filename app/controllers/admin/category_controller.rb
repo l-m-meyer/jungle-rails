bb9a286c32b44fde6d12533855c8cc1368abd188
Lisa Meyer <lisamermey@gmail.com>
2022-05-06 01:01:50 -0400

    Generate Admin::CategoryController, refactor category route

---
+++ app/controllers/admin/category_controller.rb
@@ -0,0 +1,10 @@
+class Admin::CategoryController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   get 'about/index'
   get 'static_pages/help'
   get 'static_pages/faq'
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :category, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
