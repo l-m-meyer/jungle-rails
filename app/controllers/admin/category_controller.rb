3c917412c42ca5d2a0a58e21501ae98ef8f4b0e8
Lisa Meyer <lisamermey@gmail.com>
2022-05-06 19:05:25 -0400

    New category form not working

--- app/controllers/admin/category_controller.rb
+++ app/controllers/admin/category_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoryController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
     @categories = Category.all
     @category_list = @categories.map(&:name)
@@ -9,5 +12,20 @@ class Admin::CategoryController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :category, :index], notice: 'Category saved!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
