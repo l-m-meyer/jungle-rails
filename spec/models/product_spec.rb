5e4cd863558406ef4ff2b7caeff95c17cfc02e00
Lisa Meyer <lisamermey@gmail.com>
2022-05-09 18:08:30 -0400

    Add validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'should save a valid product' do
+      @category = Category.create(name: 'Containers')
+      product = Product.new(
+        name: 'Terracotta Pots',
+        price: 35.00,
+        category_id: @category.id,
+        quantity: 20
+      )
+
+      expect(product).to be_valid
+    end
+
+    it 'should not save without a valid name' do
+      @category = Category.create(name: 'Fresh Air')
+      name = Product.new(
+        name: nil,
+        price: 20.00,
+        category_id: @category.id,
+        quantity: 11
+      )
+
+      expect(name).to_not be_valid
+    end
+  end
 end
