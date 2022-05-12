d837764347d77ecccf77415b80afc1865e890bb1
Lisa Meyer <lisamermey@gmail.com>
2022-05-12 14:24:49 -0400

    Add test that navigates to a product detail page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,18 @@
+describe('Home Page', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Navigates directly to a product detail page", () => {
+    cy.visit('products/2');
+  });
+})
