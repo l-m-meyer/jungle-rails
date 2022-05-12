f17f2066f50aeb666d70d94226d591773c831c9b
Lisa Meyer <lisamermey@gmail.com>
2022-05-11 22:30:01 -0400

    Add cypress tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Home Page', () => {
+
+  it("Goes to the homepage", () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
